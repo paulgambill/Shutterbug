fc30746b667388a183fe52416465cac551222859
Paul Gambill <pgambill@ubermind.com>
2012-05-31 09:35:22 -0700

    Created RecentPhotosTVC and set the TVC in storyboard

---
+++ Shutterbug/RecentPhotosTVC.h
@@ -0,0 +1,13 @@
+//
+//  RecentPhotosTVC.h
+//  Shutterbug
+//
+//  Created by Paul Gambill on 5/31/12.
+//  Copyright (c) 2012 Deloitte | Übermind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecentPhotosTVC : UITableViewController
+
+@end
