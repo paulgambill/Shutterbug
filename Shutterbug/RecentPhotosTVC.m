dc1a3988d2d75fff04a7c36f128a3aaa190fb6b2
Paul Gambill <pgambill@ubermind.com>
2012-06-06 12:33:46 -0700

    Fixed orientation

--- Shutterbug/RecentPhotosTVC.m
+++ Shutterbug/RecentPhotosTVC.m
@@ -66,7 +66,7 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
 }
 
 #pragma mark - Table view data source
