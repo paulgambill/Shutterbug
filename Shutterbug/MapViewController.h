e809d4b12f1e1a4cf07851f699fe140f73d2cefa
Paul Gambill <pgambill@ubermind.com>
2012-06-13 11:34:24 -0700

    Added MapViewController class

---
+++ Shutterbug/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  Shutterbug
+//
+//  Created by Paul Gambill on 6/13/12.
+//  Copyright (c) 2012 Deloitte | Übermind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
---
+++ Shutterbug/MapViewController.m
@@ -0,0 +1,43 @@
+//
+//  MapViewController.m
+//  Shutterbug
+//
+//  Created by Paul Gambill on 6/13/12.
+//  Copyright (c) 2012 Deloitte | Übermind. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
