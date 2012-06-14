deb6879e1d9d3f086db9e869f6e03e4f2acfa717
Paul Gambill <pgambill@ubermind.com>
2012-06-14 10:15:10 -0700

    Added annotation class

---
+++ Shutterbug/FlickrPhotoAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  FlickrPhotoAnnotation.h
+//  Shutterbug
+//
+//  Created by Paul Gambill on 6/14/12.
+//  Copyright (c) 2012 Deloitte | Übermind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlickrPhotoAnnotation : NSObject
+
+@end
---
+++ Shutterbug/FlickrPhotoAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  FlickrPhotoAnnotation.m
+//  Shutterbug
+//
+//  Created by Paul Gambill on 6/14/12.
+//  Copyright (c) 2012 Deloitte | Übermind. All rights reserved.
+//
+
+#import "FlickrPhotoAnnotation.h"
+
+@implementation FlickrPhotoAnnotation
+
+@end
