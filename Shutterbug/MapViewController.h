482071f5ec4d23dda6be85dc28c7c8b4a96ef6a8
Paul Gambill <pgambill@ubermind.com>
2012-06-25 10:21:24 -0700

    connect MapViewController to mapview

--- Shutterbug/MapViewController.h
+++ Shutterbug/MapViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
 @interface MapViewController : UIViewController
-
+@property (nonatomic, strong) NSArray *annotations; // of id <MKAnnotation>
 @end
--- Shutterbug/MapViewController.m
+++ Shutterbug/MapViewController.m
@@ -8,11 +8,36 @@
 
 #import "MapViewController.h"
 
-@interface MapViewController ()
+@interface MapViewController () <MKMapViewDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
 
 @implementation MapViewController
+@synthesize mapView = _mapView;
+@synthesize annotations = _annotations;
+
+- (void)updateMapView
+{
+    if (self.mapView.annotations) {
+        [self.mapView removeAnnotations:self.mapView.annotations];
+    }
+    if (self.annotations) {
+        [self.mapView addAnnotations:self.annotations];
+    }
+}
+
+- (void)setMapView:(MKMapView *)mapView
+{
+    _mapView = mapView;
+    [self updateMapView];
+}
+
+- (void)setAnnotations:(NSArray *)annotations
+{
+    _annotations = annotations;
+    [self updateMapView];
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -31,6 +56,7 @@
 
 - (void)viewDidUnload
 {
+    [self setMapView:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
